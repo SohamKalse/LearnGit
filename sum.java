b5ecea3fbc82db4e4579de621c5262885a51b7fe
Soham Kalse <sohamkalse@gmail.com>
2022-02-24 23:27:57 +0530

    initial commit

---
+++ sum.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class sum{
+    public static void main(String[] args){
+        int a = 12;
+        int b = 23;
+        int c = a + b;
+        System.out.println("Sum of numbers is " + c);
+    }
+}
